2a69cfae8cc8452a284350e238cd748892bfd7e7
tombyrnefp <tom.byrne@getfutureproof.co.uk>
2023-06-23 13:26:47 +0100

    It's a beautiful fricking day

---
+++ README.md
@@ -0,0 +1,7 @@
+Debug practice
+
+You will need to create a .env file and add PORT and DB_URL, you will need your own elephantsql database to connect to
+
+Please do NPM Install and look at the scripts 
+
+You do not need to change the test file. All other files may have issues
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors')
+const teacherRoutes = require('./routes/teachers');
+const studentRoutes = require('./routes/students');
+const app = express();
+
+// Middleware
+
+
+// Routes
+app.use('/teachers', teacherRoutes);
+app.use('/students', studentRoutes);
+app.get('/', (req, res) => {
+    res.send('Welcome to my Candy Store')
+})
+module.exports = app;
+
